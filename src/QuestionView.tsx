23293590fa1bc3a181eae4f9af2a5c52adba3367
Javier Ferrer S-B <javierferrersb@gmail.com>
2022-06-07 17:16:26 +0200

    Added empty question view

---
+++ src/QuestionView.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./QuestionView.css";
+import BottomBar from "./BottomBar";
+
+function QuestionView() {
+    return (
+        <div className="question-view">
+            <BottomBar />
+        </div>
+    );
+}
+
+export default QuestionView;
